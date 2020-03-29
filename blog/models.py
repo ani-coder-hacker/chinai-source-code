f12c5de8fd01fccb3b06a575ae2b9ee1da84692c
Anirudh <geetha021970@gmail.com>
2020-03-29 16:52:51 +0530

    More changes for an improved interface

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
 
     author = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    image = models.CharField(max_length=300, default="https://images.unsplash.com/photo-1584713284836-873df2d32409?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60")
+    image = models.CharField(max_length=300, default="https://picsum.photos/700.webp?blur")
     category = models.CharField(
     max_length=2,
     choices=CAT_CHOICES,
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,42 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post, Message
+from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all().order_by('-published_date')
+    posts_list = Post.objects.all().order_by('-published_date')
+    paginator = Paginator(posts_list, 10)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     message = Message.objects.all()
     args = {
     'posts':posts,
     'message':message
     }
     return render(request, 'blog/post_list.html', args)
+
+def search(request):
+    posts = Post.objects.all().order_by('-published_date')
+    query = request.GET.get("q")
+    if query:
+        posts = posts.filter(
+            Q(title__icontains=query) |
+            Q(author__icontains=query) |
+            Q(content__icontains=query) |
+            Q(batch__icontains=query)
+            ).distinct()
+    message = Message.objects.all()
+    args = {
+    'posts':posts,
+    'message':message
+    }
+    return render(request, 'blog/search.html', args)
 
 def home(request):
     message = Message.objects.all()
@@ -88,3 +115,7 @@ def about(request):
 def cat(request):
     args = {}
     return render(request, 'blog/categories.html', args)
+
+def contribute(request):
+    args = {}
+    return render(request, 'blog/contribute.html', args)
